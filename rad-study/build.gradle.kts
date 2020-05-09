d9f029d9401a352d948a53062f7e860c5665fd8f
Boonoboo <mathiasboonoboo@gmail.com>
2020-05-09 12:27:00 +0200

    Added rad-study project for qualitative study of rad applicability.

---
+++ rad-study/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "dk.cachet.rad"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    /* TODO: Add Ktor dependencies here */
+
+}
